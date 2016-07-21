4d5b09dd5b78deb2d6bc8ab3a4ec563088a496dc
Dermenji <Princess_Alice2016>
2016-07-21 13:38:03 +0300

    Delele Calendar

--- src/com/sirma/objectmodel/Measurements.java
+++ src/com/sirma/objectmodel/Measurements.java
@@ -4,10 +4,9 @@ import java.util.Calendar;
 import java.util.Date;
 
 public abstract class Measurements {
-    private Rainfall rainfall;
-    private Calendar interval;
     private Date date;
     private City city;
+    private Rainfall rainfall;
     private Temperature temp;
     private AirHumidity airHumidity;
     private WindSpeed windSpeed;
