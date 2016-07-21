97736157f99d47109c51c5f3c12185615c39d611
Dermenji <Princess_Alice2016>
2016-07-21 13:45:28 +0300

    New DAO

--- src/com/sirma/dao/MeasurmentsDAO.java
+++ src/com/sirma/dao/MeasurmentsDAO.java
@@ -3,18 +3,14 @@ package com.sirma.dao;
 
 import java.util.List;
 
-public interface FactorsDAO<E, K> {
+public interface MeasurmentsDAO<E, K> {
     List<E> getAll();
 
     E update(E entity);
 
     E add(E entity);
 
-    E getEntityById(K id);
-
     E getByName(K name);
-
-    boolean remove(K id);
 
     boolean create(E entity);
 }
