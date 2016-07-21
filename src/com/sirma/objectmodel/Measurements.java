dc31572f8b359d9f010bdba6e15394eb4951012e
Dermenji <Princess_Alice2016>
2016-07-21 13:27:55 +0300

    Change object model

--- src/com/sirma/objectmodel/Measurements.java
+++ src/com/sirma/objectmodel/Measurements.java
@@ -11,4 +11,5 @@ public abstract class Measurements {
     private Temperature temp;
     private AirHumidity airHumidity;
     private WindSpeed windSpeed;
+    private WindDirection windDirection;
 }
